81363124569e774a2bc78a1240ec5112563c92c2
kevinta893 <kevinta893@gmail.com>
2017-08-30 13:40:10 -0600

    Added start of Unity Port project

---
+++ NetworkItUnity/Assets/NetworkIt/Scripts/NetworkItUnity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NetworkIt;
+
+public class NetworkItUnity : MonoBehaviour {
+
+    public string urlAddress;
+    public int port = 8000;
+    public string username;
+
+    private Client connection;
+
+	void Start () {
+
+        connection = new Client(username, urlAddress, port);            //create and establish connection to server
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
