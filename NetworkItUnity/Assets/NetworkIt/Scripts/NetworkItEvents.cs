dfc5f3fd2e093b445c4600a4c75818081da823b2
kevinta893 <kevinta893@gmail.com>
2017-08-30 14:23:04 -0600

    Unity Demo scene

---
+++ NetworkItUnity/Assets/NetworkIt/Scripts/NetworkItEvents.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NetworkIt;
+
+public class NetworkItEvents : MonoBehaviour {
+
+    public string urlAddress = "";
+    public int port = 58181;
+    public string username = "give me a new name";                                    
+
+    public GameObject[] messageListeners;
+
+    private Client connection;
+
+    void Start() {
+
+        if (urlAddress == null || urlAddress.Length <= 0) { 
+            Debug.LogError("URL or IP Address required.");
+            return;
+        }
+
+        if (username == null || username.Length <= 0) { 
+            Debug.LogError("Username required.");
+            return;
+        }
+
+        connection = new Client(username, urlAddress, port);            //create and establish connection to server
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void NotifyMessageListeners(Message recievedMessage)
+    {
+        Debug.Log("Message recieved:\n" + recievedMessage);
+
+        foreach (GameObject g in messageListeners)
+        {
+            g.BroadcastMessage("OnNetworkMessage", recievedMessage);
+        }
+
+    }
+
+}
