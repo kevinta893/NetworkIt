e91073fb9ee6d6885e21e8d21bb66fee76ae0f3b
kevinta893 <kevinta893@gmail.com>
2017-09-07 23:30:37 -0600

    Socket.io works in java

--- NetworkItJava/src/Main.java
+++ NetworkItJava/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 	public static void main(String[] args)
 	{
-		Client client = new Client("demo_username", "http://www.kyubeypawnshop.xyz", 8000);
+		Client client = new Client("demo_test_username", "http://www.kyubeypawnshop.xyz", 8000);
 	}
 }
--- NetworkItJava/src/networkit/Client.java
+++ NetworkItJava/src/networkit/Client.java
@@ -1,19 +1,71 @@
 package networkit;
 
 import java.net.URISyntaxException;
+
+import org.json.JSONObject;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter.Listener;
 
 public class Client {
 
-	public Client(){
+	
+	private String username = "demo_test_username";
+	private String url = "http://581.cpsc.ucalgary.ca";
+	private int port = 8000;
+	
+	private Socket client;
+	
+	public Client()
+	{
 		
 	}
 	
-	public Client(String username, String url, int port){
-		try {
-			 Socket socket = IO.socket(url + ":" + port);
-			 socket.connect();
+	public Client(String username, String url, int port)
+	{
+		try 
+		
+		{
+			 this.client = IO.socket(url + ":" + port);
+			 
+			 this.client.on(Socket.EVENT_CONNECT, new Listener() 
+			 {
+				@Override
+				public void call(Object... args) 
+				{
+					JSONObject connectMsg = new JSONObject();
+					connectMsg.put("username", username);
+					client.emit("client_connect", connectMsg);
+					System.out.println("Connection successful.");
+				}
+			 });
+			 
+			 this.client.on(Socket.EVENT_MESSAGE, new Listener() {
+				
+				@Override
+				public void call(Object... args) {
+					System.out.println((args[0].toString()));
+				}
+			 });
+			 
+			 this.client.on(Socket.EVENT_DISCONNECT, new Listener() {
+					
+				@Override
+				public void call(Object... args) {
+					System.out.println("Client disconnected.");
+				}
+			 });
+			 
+			 this.client.on(Socket.EVENT_ERROR, new Listener() {
+					
+				@Override
+				public void call(Object... args) {
+					System.out.println("Error occured!");
+				}
+			 });
+			 
+			 this.client.connect();
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
