21282de05c2f8db2c89193d0ea5935ebd2c9646a
kevinta893 <kevinta893@gmail.com>
2017-09-27 18:07:25 -0600

    Python-Pi connects, sends and recieves messages
    Sending message is still awkward but at least something happens

---
+++ NetworkItPi-Python/networkit.py
@@ -0,0 +1,163 @@
+import json
+import logging
+from threading import Thread
+#Uses socket.io v0.7.5- Nexus (i.e. a beta for Socket.io 2.0 servers)
+#https://github.com/invisibleroads/socketIO-client
+#https://github.com/nexus-devs/socketIO-client      (beta branch)
+
+
+
+
+
+class Client:
+
+    username = "demo_test_username"
+    url = "581.cpsc.ucalgary.ca"
+    port = 8000
+
+    socket = None
+    socket_thread = None
+
+    EVENT_MESSAGE = "message"
+    EVENT_CONNECT = "connect"
+    EVENT_DISCONNECT = "disconnect"
+    EVENT_ERROR = "error"
+
+
+    def __init__(self, username, url, port):
+
+        self.username = username
+        self.url = url
+        self.port = port
+
+
+        #Setup events
+        self.subscribers = {}
+        self.subscribers[self.EVENT_MESSAGE] = []
+        self.subscribers[self.EVENT_CONNECT] = []
+        self.subscribers[self.EVENT_DISCONNECT] = []
+        self.subscribers[self.EVENT_ERROR] = []
+
+
+
+    def start_connection(self):
+        #if already connected, disconnect
+
+        if (self.socket != None):
+            print "Already connected!"
+            return;
+
+        # start up logger
+        logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
+        logging.basicConfig()
+
+        # setup networking
+        from socketIO_client_nexus import SocketIO, LoggingNamespace, BaseNamespace
+        self.socket = SocketIO(self.url, self.port, BaseNamespace, wait_for_connection=False)
+
+
+        #events
+        self.socket.on('connect', self.on_connect)
+        self.socket.on('disconnect', self.on_disconnect)
+        self.socket.on('error', self.on_socket_error)
+        self.socket.on('message', self.on_message)
+
+        self.socket.emit('client_connect', {
+            'username': self.username,
+            'platform': "Python 2.7"
+        })
+        self.socket_thread = Thread(target = self.wait_on_socket)
+        self.socket_thread.start()
+
+    def close_connection(self):
+        if (self.socket != None):
+            self.socket.disconnect()
+
+    def wait_on_socket(self):
+        self.socket.wait()
+        print "SocketIO client thread terminated"
+
+    def send_message(self, message):
+        sendMessage = {
+            'username' : self.username,
+            'deliverToSelf' : message.deliverToSelf,
+            'subject' : message.subject
+            #'fields' : message.fields
+        }
+        self.socket.emit('message', sendMessage)
+
+    #==========================
+    #Events
+
+    def on_connect(self, args):
+        print "Connection Sucessful"
+        self.socket.emit('client_connect', {
+            'username': self.username,
+            'platform': "Python 2.7"
+        })
+        self.emit_event(self.EVENT_CONNECT, None)
+
+
+
+    def on_disconnect(self, args):
+        self.emit_event(self.EVENT_DISCONNECT, None)
+        print "Client Disconnected"
+
+    def on_socket_error(self, err):
+        print "Error!"
+        print err
+        self.emit_event(self.EVENT_ERROR, err)
+
+
+
+    def on_message(self, data):
+        self.emit_event(self.EVENT_MESSAGE, data)
+
+
+
+    #=========
+    #events external
+
+
+    def register_listener(self, event_name, callback):
+        self.subscribers[event_name].append(callback)
+
+    def emit_event(self, event_name, args):
+        for i in range(0, len(self.subscribers[event_name])):
+            self.subscribers[event_name][i](args)
+
+
+
+#=======================
+
+
+class Message:
+
+    def __init__(self, subject):
+        self.subject = subject
+        self.deliverToSelf = False
+        self.fields = []
+
+    def add_field(self, key, value):
+        self.fields = Field(key, value)
+
+    def get_field(self, key):
+        for i in range(0, len(fields)):
+            if (self.fields[i].key == key):
+                return fields[i].value
+
+        return None
+
+    def to_string(self):
+        return "NOT IMPLEMENTED YET"
+
+
+
+class Field:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def to_string(self):
+        self.key + " : " + str(self.value);
---
+++ NetworkItPi-Python/networkit_demo.py
@@ -0,0 +1,80 @@
+from networkit import Client, Message, Field
+
+from Tkinter import Tk, Label, Button, Frame
+import tkMessageBox
+
+
+class MainWindow(Frame):
+
+    count =0
+
+    def __init__(self, master=None):
+
+        self.server = None
+
+        #setup window
+        self.mainWindow = Tk()
+
+        self.btnConnect = Button(self.mainWindow, text="Connect", command=self.connect_button)
+        self.btnConnect.place(x=0, y=0)
+
+        self.btnSend = Button(self.mainWindow, text="Send poke", command=self.send_poke)
+        self.btnSend.place(x=0, y=30)
+
+
+        self.mainWindow.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.mainWindow.mainloop()
+
+
+
+
+
+
+    def connect_button(self):
+
+        # setup network
+        self.server = Client("demo_test_username", "192.168.1.104", 8000)
+        self.server.register_listener(Client.EVENT_MESSAGE, self.on_message)
+        self.server.register_listener(Client.EVENT_CONNECT, self.on_connect)
+        self.server.register_listener(Client.EVENT_DISCONNECT, self.on_disconnect)
+        self.server.register_listener(Client.EVENT_ERROR, self.on_error)
+        self.server.start_connection()
+
+
+    def send_poke(self):
+        message = Message("Poke!")
+        message.add_field("num1", str(1))
+        message.add_field("num2", str(4))
+        self.count += 1
+        message.add_field("count", str(self.count))
+        self.server.send_message(message)
+
+
+    def on_message(self, args):
+        print "Message recieved" + str(args)
+
+    def on_connect(self):
+        print "Client Connected"
+
+    def on_disconnect(self):
+        print "Client Disconnected"
+
+    def on_error(self, args):
+        print "Error!" + str(args)
+
+
+
+
+    def on_closing(self):
+        # on close window, disconnect from server
+        if (self.server != None):
+            self.server.close_connection()
+        self.mainWindow.destroy()
+
+
+#Launch the main window
+MainWindow()
+
+
+
