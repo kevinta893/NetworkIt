af4f398bf88268cf345bf0935e076b488f8959a6
kevinta893 <kevinta893@gmail.com>
2017-11-09 18:51:11 -0700

    Cube rotates about the Wrist-Thumb

--- PlatformDemos/NetworkItUnity+KinectV2/NetworkItUnity+KinectV2/Assets/Scripts/BodyNetworkTemplate.cs
+++ PlatformDemos/NetworkItUnity+KinectV2/NetworkItUnity+KinectV2/Assets/Scripts/BodyNetworkTemplate.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NetworkIt;
 
 public class BodyNetworkTemplate : MonoBehaviour {
 
     private List<BodyGameObject> bodies = new List<BodyGameObject>();
 
+    private MeshRenderer mesh;
 	// Use this for initialization
 	void Start () {
-		
+        mesh = GetComponent<MeshRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -18,12 +22,38 @@ public class BodyNetworkTemplate : MonoBehaviour {
         {
             //some bodies, send orientation update
             GameObject thumbRight = bodies[0].GetJoint(Windows.Kinect.JointType.ThumbRight);
-            Debug.Log(thumbRight.transform.localPosition*100);
+            GameObject handRight = bodies[0].GetJoint(Windows.Kinect.JointType.HandRight);
+            GameObject handTipRight = bodies[0].GetJoint(Windows.Kinect.JointType.HandTipRight);
 
-            this.transform.rotation = Quaternion.Euler(thumbRight.transform.localPosition*50);
+            float angle = VerticalWristRotation(
+                thumbRight.transform.position,
+                handRight.transform.position,
+                handTipRight.transform.position
+                );
+            Debug.Log(angle);
+            this.transform.rotation = Quaternion.Euler(0, angle, 0);
         }
     }
 
+    /// <summary>
+    /// Gets an angle between 3 points that form a connection:  p1---p2---p3
+    /// Such that the vector v = p3-p2 defines the angle around p1
+    /// </summary>
+    /// <param name="p1">A position in space</param>
+    /// <param name="p2">A position in space</param>
+    /// <param name="p3">A position in space</param>
+    /// <returns></returns>
+    float VerticalWristRotation(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        Vector3 a = p2 - p3;
+        Vector3 b = p2 - p1;
+        a.Normalize();
+        b.Normalize();
+        Vector3 thetaVector = Vector3.ProjectOnPlane(b, a);
+        thetaVector.Normalize();
+
+        return Vector3.SignedAngle(Vector3.right, thetaVector, Vector3.up);
+    }
 
 
     void Kinect_BodyFound(object args)
@@ -46,5 +76,39 @@ public class BodyNetworkTemplate : MonoBehaviour {
                 }
             }
         }
+    }
+
+
+
+    public void NetworkIt_Message(object m)
+    {
+        //TODO your code here
+        Message message = (Message)m;
+
+        int count = 0;
+        int.TryParse(message.GetField("count"), out count);
+
+        this.transform.rotation = Quaternion.Euler(0, count * 10, 0);
+    }
+
+    public void NetworkIt_Connect(object args)
+    {
+        //TODO your code here
+        EventArgs eventArgs = (EventArgs)args;
+        mesh.material.color = new Color(0.0f, 1.0f, 0.0f);
+    }
+
+    public void NetworkIt_Disconnect(object args)
+    {
+        //TODO your code here
+        EventArgs eventArgs = (EventArgs)args;
+        mesh.material.color = new Color(1.0f, 0.0f, 0.0f);
+    }
+
+    public void NetworkIt_Error(object err)
+    {
+        //TODO your code here
+        ErrorEventArgs error = (ErrorEventArgs)err;
+        Debug.LogError(error);
     }
 }
