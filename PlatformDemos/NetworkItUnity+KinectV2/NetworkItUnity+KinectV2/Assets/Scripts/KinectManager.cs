dd7ac297ae59cbec4d5ef01ac0fd4c32398d1652
kevinta893 <kevinta893@gmail.com>
2017-11-13 18:30:45 -0700

    Face tracking works!
    But it's real slow man

--- PlatformDemos/NetworkItUnity+KinectV2/NetworkItUnity+KinectV2/Assets/Scripts/KinectManager.cs
+++ PlatformDemos/NetworkItUnity+KinectV2/NetworkItUnity+KinectV2/Assets/Scripts/KinectManager.cs
@@ -99,7 +99,76 @@ public class KinectManager : MonoBehaviour {
 
 
         //demo code, comment out or remove as necessary
+        //Demo code and many more samples for OpenCVSharp can be found at: https://github.com/VahidN/OpenCVSharp-Samples
         DemoIRBlobTrack();
+        DemoFaceTrack();
+    }
+
+    private CascadeClassifier cascade = new CascadeClassifier(@"Assets\OpenCVSharp-v3.2\Data\haarcascade_frontalface_alt.xml");
+    private CascadeClassifier nestedCascade = new CascadeClassifier(@"Assets\OpenCVSharp-v3.2\Data\haarcascade_eye_tree_eyeglasses.xml");
+
+    private void DemoFaceTrack()
+    {
+        Mat colorImage = new Mat(ColorHeight, ColorWidth, MatType.CV_8UC4, _ColorRawData);              //rows=height, cols=width
+        Mat grayImage = new Mat();
+        Cv2.CvtColor(colorImage, grayImage, ColorConversionCodes.RGBA2GRAY);
+        Cv2.EqualizeHist(grayImage, grayImage);
+
+        OpenCvSharp.Rect[] faces = cascade.DetectMultiScale(
+               image: grayImage,
+               scaleFactor: 1.1,
+               minNeighbors: 2,
+               flags: HaarDetectionType.DoRoughSearch | HaarDetectionType.ScaleImage,
+               minSize: new Size(30, 30)
+               );
+
+        
+        for (int i =0; i < faces.Length; i++)
+        {
+            OpenCvSharp.Rect faceRect = faces[i];
+
+            //outline overall face in image
+            var rndColor = Scalar.FromRgb(
+                UnityEngine.Random.Range(0, 255), 
+                UnityEngine.Random.Range(0, 255), 
+                UnityEngine.Random.Range(0, 255)
+                );
+            Cv2.Rectangle(colorImage, faceRect, rndColor, 3);
+
+            //now do nested features like the eyes
+            Mat subFaceImage = new Mat(colorImage, faceRect);
+            Mat detectedFaceGrayImage = new Mat();
+            Cv2.CvtColor(subFaceImage, detectedFaceGrayImage, ColorConversionCodes.RGBA2GRAY);
+            var nestedObjects = nestedCascade.DetectMultiScale(
+                image: detectedFaceGrayImage,
+                scaleFactor: 1.1,
+                minNeighbors: 2,
+                flags: HaarDetectionType.DoRoughSearch | HaarDetectionType.ScaleImage,
+                minSize: new Size(30, 30)
+                );
+
+            //display each nested face feature
+            foreach (var nestedObject in nestedObjects)
+            {
+                var center = new Point
+                {
+                    X = (int)(Math.Round(nestedObject.X + nestedObject.Width * 0.5, MidpointRounding.ToEven) + faceRect.Left),
+                    Y = (int)(Math.Round(nestedObject.Y + nestedObject.Height * 0.5, MidpointRounding.ToEven) + faceRect.Top)
+                };
+                var radius = Math.Round((nestedObject.Width + nestedObject.Height) * 0.25, MidpointRounding.ToEven);
+                Cv2.Circle(colorImage, center, (int)radius, rndColor, thickness: 3);
+            }
+
+        }
+
+
+        //Convert back to RGBA32
+        //Mat faceImageOut = new Mat(IRWidth, IRHeight, MatType.CV_8UC4);
+        //Cv2.CvtColor(colorImage, faceImageOut, ColorConversionCodes.RGBA2RGBA);
+
+        //load onto texture
+        _ColorTexture.LoadRawTextureData(ConvertMatToBytes(colorImage));
+        _ColorTexture.Apply();
     }
 
 
